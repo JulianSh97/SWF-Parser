ef43e8080ce3be14219bb331902810abba32d09a
JulianSh97 <jshaer02@campus.haifa.ac.il>
2020-11-16 15:07:35 +0200

    l

--- MatlaLog_Parser.py
+++ MatlaLog_Parser.py
@@ -9,6 +9,7 @@ cleaned_log_path="log_files/Cleaned_Matlab_Log.txt"
 SWF_log = "log_files/SWF_Matlab_Log.txt"
 UserDict=dict()
 ApplicationsDict=dict()
+Interarrivals1=list()
 with open (cleaned_log_path) as file:  # Generating a dictionary containing the user name (e.g user1@computer1) as a key and a list of 
     for line in file:                  # all the jobs he is involved in
         line_split=line.split()
@@ -35,15 +36,26 @@ with open (original_log_path) as file:
     
 job_number = 1
 PrecedingJobNum=0
+PrecedingJobArrival=started_time
 row_counter=0
+Interarrivals=dict()
+Interarrivals2=list()
 with open(SWF_log, "w") as swf_file:
     for row in log_file.readlines():
         row_split_list = row.split()
-        if "IN:" in row_split_list:     # we only read lines with "OUT"
-            continue
         arrival_time=datetime.strptime(row_split_list[0], "%H:%M:%S").time()                # calculate the arrival time of the job
         arrival_time=(arrival_time.hour)*3600+(arrival_time.minute)*60+(arrival_time.second)# and converting it to seconds
+        secs=arrival_time-started_time
+        Interarrivals1.append(secs)
+        if secs in Interarrivals.keys():
+            Interarrivals[secs]+=1
+        else:
+             Interarrivals2.append(secs)
+             Interarrivals.setdefault(secs,1)
+        if "IN:" in row_split_list:     # we only read lines with "OUT"
+            continue
         submit_time=arrival_time-started_time  # first column in the SWF fields sumbit time
+        started_time=arrival_time
         currentUser=UserDict[row_split_list[4]] # pull the list that contains all the jobs done by this user
         for job in currentUser:
             if  "IN: "+ row_split_list[3] in job:                                # search for the "IN" request of the same job we have now
@@ -51,10 +63,10 @@ with open(SWF_log, "w") as swf_file:
                 endTime=(endTime.hour)*3600+(endTime.minute)*60+(endTime.second) # of the "IN" time and "OUT" time ;not sure about it
         runtime=endTime-arrival_time
         NumOfProc=-1 #couldn't find information about the number of processors requested
-        AverageCPUtime=runtime #not sure
+        AverageCPUtime=-1 #not sure
         AverageMem=-1 # no information found
         RequestedProc=1 
-        Requestedruntime=runtime 
+        Requestedruntime=-1 
         RequestedMem=-1
         Status=1
         UserID=list(UserDict.keys()).index(row_split_list[4]) #get the index of the user from the keys of the users dictionary
@@ -66,9 +78,113 @@ with open(SWF_log, "w") as swf_file:
         current_row = RowClass(order=job_number, submit_time=submit_time,wait_time=0, runtime=runtime, number_of_nodes=NumOfProc,average_cpu_time=AverageCPUtime,average_memory_per_node=AverageMem,requested_processors=RequestedProc,requested_runtime=Requestedruntime,
                                requested_memory=RequestedMem,status=Status,user_id=UserID, group_id=GroupID,application_id=Executable, number_of_queues=Queue,number_of_partitions=Partition,preceding_job_number=PrecedingJobNum,think_time=ThinkTime)
         swf_file.write(current_row.convert_to_string())
+    
         #TODO write to the SWF file - ask about the date and time
         job_number+=1
         PrecedingJobNum+=1
 #Lines like 156-182 in the original file shouldn't be removed due to description of memory used, runtime in ms...
-#information may be lost (not sure about this thing) should discuss it with the group
-        
+#information may be lost (not sure about this thing) should discuss it with the group    
+    
+Probabilities=list()
+Interarrivals2.sort()
+vals=list()
+for v in Interarrivals2:
+    vals.append(Interarrivals[v])
+    
+for v in vals:
+    Probabilities.append(v/len(Interarrivals1))
+#here we show the probabilities graph
+plt.title("Probability(y) of Interarrivals times in seconds(x)")
+plt.xscale('log',basex=2)
+plt.plot(Interarrivals2, Probabilities)
+plt.xlim(1, 2 ** 12)
+plt.ylim(0, 0.07)
+plt.xlabel('Interarrival (seconds)')
+plt.ylabel('PDF')
+plt.show()
+
+
+#here we show the CDF grph
+Probabilities2=list()
+Interarrivals3=list()
+i=0
+while i<max(Interarrivals1):
+    p=0
+    j=0
+    Interarrivals3.append(i)
+    while Interarrivals2[j]<i:
+        p+=Probabilities[j]
+        j+=1
+    i+=1    
+    Probabilities2.append(p)
+plt.title("Cumulative Probability(y) of Interarrivals times in seconds(x)")    
+plt.xscale('log',basex=2)    
+plt.plot(Interarrivals3, Probabilities2)
+plt.xlim(1, 2**13)
+plt.ylim(0, 1)
+plt.xlabel('Interarrival (seconds)')
+plt.ylabel('Comulative probability')
+plt.show()
+    
+RunTimes=list()
+RunTimes2=list()
+RunTimesDict=dict()
+RunTimesProbabilities=list()
+RunTimesProbabilities2=list()
+
+def  CreateRunTimes():
+    SWF=open(SWF_log,"r")
+    for row in SWF:
+        row=row.split()
+        runTime=int(row[3])
+        RunTimes.append(runTime)
+    for runTime in RunTimes:
+        if runTime in RunTimesDict.keys():
+            RunTimesDict[runTime]+=1
+        else:
+            RunTimes2.append(runTime)
+            RunTimesDict.setdefault(runTime,1)
+    SWF.close()
+    
+CreateRunTimes()
+
+RunTimes2=sorted(RunTimes2)
+vals=list()
+
+for r in RunTimes2:
+    vals.append(RunTimesDict[r])
+
+for v in vals:
+    RunTimesProbabilities.append(v/len(RunTimes))
+vals=list()    
+plt.title("Probability(y) of Run Times in seconds(x)")    
+plt.xscale('log',basex=2)
+plt.plot(RunTimes2, RunTimesProbabilities)
+plt.xlim(1, 2**16)
+plt.ylim(0, 0.05)
+plt.xlabel('Run Time (seconds)')
+plt.ylabel('probability')
+plt.show()
+i=0
+RunTimes3=list()
+RunTimes3O=list()
+while i<max(RunTimes):
+    p=0
+    j=0
+    RunTimes3.append(i)
+    while RunTimes2[j]<i:
+        p+=RunTimesProbabilities[j]
+        j+=1
+    i+=1    
+    RunTimesProbabilities2.append(p)
+
+i=0
+plt.title("Cumulative probability(y) of Run Times in seconds(x)")     
+plt.xscale('log',basex=2)
+#plt.plot(RunTimes3O, RunTimesProbabilities2O,label="Other Jobs")
+plt.plot(RunTimes3, RunTimesProbabilities2)
+plt.xlim(1, 2**16)
+plt.ylim(0, 1)
+plt.xlabel('Run Time (seconds)')
+plt.ylabel('Cumulative probability')
+plt.show()
--- main_parser.py
+++ main_parser.py
@@ -2,8 +2,8 @@ from shutil import copyfile, copy
 import os
 from datetime import datetime, date
 from RowClass import RowClass
-import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt 
+import numpy as np
 # datetime, order, submit_time, runtime, number_of_nodes, user_id, group_id,
 # application_id, number_of_queues, wait_time=0, average_cpu_time=-1, average_memory_per_node=-1,
 # requested_processors=-1, requested_runtime=-1, requested_memory=-1, status=-1, number_of_partitions=-1,
@@ -26,7 +26,11 @@ log_file.seek(0)
 
 started_time = datetime.strptime(first_row.split()[4] + " " + first_row.split()[5], "%m/%d/%y %H:%M:%S")
 
-RuntimeHist=dict()
+Interarrivals=dict()
+Interarrivals2=list()
+Interarrivals1=list()
+
+
 
 
 with open(SWF_log, "w") as swf_file:
@@ -39,12 +43,7 @@ with open(SWF_log, "w") as swf_file:
         submit_time = datetime.combine(date.today(), date_and_time.time()) - datetime.combine(date.today(),
                                                                                               started_time.time())  # subtract current time from start time to get time from beginning
         runtime = row_split_list[3]
-        
-        #RuntimeHist.append(runtime)
-        if runtime in RuntimeHist.keys():
-            RuntimeHist[runtime]+=1
-        else:
-           RuntimeHist.setdefault(runtime,1)
+
         number_of_nodes = row_split_list[2]
 
         user_id = row_split_list[0]
@@ -64,7 +63,176 @@ with open(SWF_log, "w") as swf_file:
         swf_file.write(current_row.convert_to_string())
 
         row_counter += 1
-plt.bar(RuntimeHist.keys(),RuntimeHist.values())
-plt.xlabel("Number of jobs")
-plt.ylabel("Runtime")
+        
+
+
+Interarrivals=dict()
+Interarrivals2=list()
+Interarrivals1=list()
+def CreateInterarrivalsDict():
+    SWF=open(SWF_log,"r")
+    for row in SWF:
+        row=row.split()
+        secs=datetime.strptime(row[18]+' '+row[19], '%Y-%m-%d %H:%M:%S')
+        Interarrivals1.append(secs)
+    i=1;
+    while i<len(Interarrivals1):
+        Interarrivals1[i-1]=(Interarrivals1[i]-Interarrivals1[i-1]).total_seconds()
+        i+=1
+    Interarrivals1.remove(Interarrivals1[i-1])  
+    for secs in Interarrivals1:
+        if secs in Interarrivals.keys():
+            Interarrivals[secs]+=1
+        else:
+            Interarrivals2.append(secs)
+            Interarrivals.setdefault(secs,1)
+    SWF.close()
+    
+    
+CreateInterarrivalsDict()
+Probabilities=list()
+Interarrivals2.sort()
+vals=list()
+for v in Interarrivals2:
+    vals.append(Interarrivals[v])
+    
+for v in vals:
+    Probabilities.append(v/len(Interarrivals1))
+#here we show the probabilities graph
+plt.title("Probability(y) of Interarrivals times in seconds(x)")
+plt.xscale('log',basex=2)
+plt.plot(Interarrivals2, Probabilities)
+plt.xlim(0, 1000)
+plt.ylim(0, 0.017)
+plt.xlabel('Interarrival (seconds)')
+plt.ylabel('PDF')
 plt.show()
+
+
+#here we show the CDF grph
+Probabilities2=list()
+Interarrivals3=list()
+i=0
+while i<52000:
+    p=0
+    j=0
+    Interarrivals3.append(i)
+    while Interarrivals2[j]<i:
+        p+=Probabilities[j]
+        j+=1
+    i+=1    
+    Probabilities2.append(p)
+plt.title("Cumulative Probability(y) of Interarrivals times in seconds(x)")    
+plt.xscale('log',basex=2)    
+plt.plot(Interarrivals3, Probabilities2)
+plt.xlim(0, 2000)
+plt.ylim(0, 1)
+plt.xlabel('Interarrival (seconds)')
+plt.ylabel('Comulative probability')
+plt.show()
+    
+RunTimes=list()
+RunTimes2=list()
+RunTimesDict=dict()
+RunTimesProbabilities=list()
+RunTimesProbabilities2=list()
+
+RunTimesS=list()
+RunTimes2S=list()
+RunTimesDictS=dict()
+RunTimesProbabilitiesS=list()
+RunTimesProbabilities2S=list()
+
+
+
+def  CreateRunTimes():
+    SWF=open(SWF_log,"r")
+    for row in SWF:
+        row=row.split()
+        runTime=int(row[3])
+        RunTimes.append(runTime)
+        if row[11] == "special":
+            RunTimesS.append(int(row[3]))
+    for runTime in RunTimes:
+        if runTime in RunTimesDict.keys():
+            RunTimesDict[runTime]+=1
+        else:
+            RunTimes2.append(runTime)
+            RunTimesDict.setdefault(runTime,1)
+            
+    for runTime in RunTimesS:
+        if runTime in RunTimesDictS.keys():
+            RunTimesDictS[runTime]+=1
+        else:
+            RunTimes2S.append(runTime)
+            RunTimesDictS.setdefault(runTime,1)    
+            
+    SWF.close()
+    
+CreateRunTimes()
+
+RunTimes2=sorted(RunTimes2)
+RunTimes2S=sorted(RunTimes2S)
+vals=list()
+
+for r in RunTimes2:
+    vals.append(RunTimesDict[r])
+
+for v in vals:
+    RunTimesProbabilities.append(v/len(RunTimes))
+vals=list()    
+
+for r in RunTimes2S:
+    vals.append(RunTimesDictS[r])
+
+for v in vals:
+    RunTimesProbabilitiesS.append(v/len(RunTimesS))
+plt.title("Probability(y) of Run Times in seconds(x)")    
+plt.xscale('log',basex=2)
+plt.plot(RunTimes2, RunTimesProbabilities,label="All Jobs")
+plt.plot(RunTimes2S, RunTimesProbabilitiesS,label="Jobs by user special")
+plt.xlim(0, 52000)
+plt.ylim(0, 0.1)
+plt.xlabel('Run Time (seconds)')
+plt.ylabel('probability')
+plt.legend()
+plt.show()
+i=0
+RunTimes3=list()
+RunTimes3O=list()
+RunTimes3S=list()
+while i<52000:
+    p=0
+    j=0
+    RunTimes3.append(i)
+    while RunTimes2[j]<i:
+        p+=RunTimesProbabilities[j]
+        j+=1
+    i+=1    
+    RunTimesProbabilities2.append(p)
+
+i=0
+while i<52000:
+    p=0
+    j=0
+    RunTimes3S.append(i)
+    while RunTimes2S[j]<i:
+        p+=RunTimesProbabilitiesS[j]
+        j+=1
+    i+=1    
+    RunTimesProbabilities2S.append(p)
+plt.title("Cumulative probability(y) of Run Times in seconds(x)")     
+plt.xscale('log',basex=2)
+#plt.plot(RunTimes3O, RunTimesProbabilities2O,label="Other Jobs")
+plt.plot(RunTimes3, RunTimesProbabilities2,label="All Jobs")
+plt.plot(RunTimes3S, RunTimesProbabilities2S,label="user special Jobs")
+plt.xlim(1, 51000)
+plt.ylim(0, 1)
+plt.xlabel('Run Time (seconds)')
+plt.ylabel('Cumulative probability')
+plt.legend()
+plt.show()
+
+print(sum(Probabilities))
+print(sum(RunTimesProbabilities))
+print(sum(RunTimesProbabilitiesS))
